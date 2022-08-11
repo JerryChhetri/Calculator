4cc8ee64693f76358550bbb58d2c980a6169a19f
PramodBohara <110480138+PramodBohara@users.noreply.github.com>
2022-08-11 12:23:07 +0400

    Update main.java

--- src/main.java
+++ src/main.java
@@ -1,1 +1,48 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String args[]) {
+        System.out.println("CALCULATOR");
+        Scanner s = new Scanner(System.in );
+        int a, b;
+        char operator;
+        System.out.print("NUM 1 : ");
+        a=s.nextInt();
+        System.out.print("NUM 2 : ");
+        b=s.nextInt();
+        System.out.print("Enter operator (+, -, *, /)");
+        operator = s.next().charAt(0);
+        double addition  = a+b;
+        double subtraction  = a-b;
+        double multiplication  = a*b;
+        double division  = a/b;
 
+        switch(operator)
+        {
+            case '+' :
+            {
+                System.out.print("Total after Addition is : "+addition);
+                break;
+            }
+            case '-' :
+            {
+                System.out.print("Total after Subtraction is : " +subtraction);
+                break;
+            }
+            case '*' :
+            {
+                System.out.print("Total after Multiplication is : "+multiplication);
+                break;
+            }
+            case '/' :
+            {
+                System.out.print("Total after Division is : "+division);
+                break;
+            }
+            default :
+            {
+                System.out.print("Please select proper operator");
+                return;
+            }
+        }
+    }
+}
